0fbc210b3db6caf982d372043f161fcf2f7ccdde
Igor Petrenko <igor06.03.1995@gmail.com>
2020-08-26 01:17:00 +0300

    Корректировка файлов-миграций

--- migrations/00001_create_users_table.sql
+++ migrations/00001_create_users_table.sql
@@ -2,7 +2,7 @@ create table if not exists `users` (
     `user_id` int(10) unsigned not null auto_increment,
     `firstname` varchar(255) not null,
     `lastname` varchar(255) not null,
-    `avatar` int(10) unsigned not null,
+    `avatar` varchar(255) not null,
     `created` timestamp not null default now(),
     `is_admin` boolean not null,
     primary key (user_id)
--- migrations/00006_create_images_table.sql
+++ migrations/00006_create_images_table.sql
@@ -1,5 +1,5 @@
 create table if not exists `images` (
-    `image_id` int(10) unsigned not null auto_increment,
+    `image_id` varchar(255) not null,
     `news_id` int(10) unsigned not null,
     primary key (image_id),
     foreign key (news_id) references news (news_id)
