b4a0154764cc6659d5965fab178db279134db6a7
Igor Petrenko <igor06.03.1995@gmail.com>
2020-08-29 17:39:21 +0300

    Приведен интерфейс абстрактного класса

---
+++ api/TableInterface.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Api;
+
+/**
+ * Интерфейс для базового взаимодействия с таблицами базы данных
+ */
+interface TableInterface
+{
+    /**
+     * Обработка GET-запроса.
+     *
+     * @return string
+     */
+    public function getRequest(): string;
+
+    /**
+     * Обработка POST-запроса.
+     *
+     * @return string
+     */
+    public function createElement(): string;
+
+    /**
+     * Обработка PUT-запроса
+     *
+     * @param array $putParams
+     *
+     * @return string
+     */
+    public function updateElement($putParams): string;
+
+    /**
+     * Обработка DELETE-запроса
+     *
+     * @return string
+     */
+    public function deleteElement(): string;
+}
