4f317931834ba2697233b56e9414bccb821776a8
Igor Petrenko <igor06.03.1995@gmail.com>
2020-09-26 22:23:52 +0300

    Добавлена единая точка входа

--- public/router.php
+++ public/router.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$uri = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
+$uri = '/' . explode('/', $_SERVER['SCRIPT_NAME'], 3)[1];
 
 switch ($uri) {
     case '/':
@@ -11,12 +11,14 @@ switch ($uri) {
     case '/authors':
         include_once 'api/authors/index.php';
         break;
+    case '/categories':
+        include_once 'api/categories/index.php';
+        break;
     case '/drafts':
         include_once 'api/drafts/index.php';
         break;
     case '/news':
         include_once 'api/news/index.php';
-        //echo 'News';
         break;
     case '/tags':
         include_once 'api/tags/index.php';
@@ -26,5 +28,4 @@ switch ($uri) {
         break;
     default:
         return false;
-        break;
 }
