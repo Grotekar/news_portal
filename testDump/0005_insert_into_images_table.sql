b5fe5ea4c1e3f4e2fe0a468b19f8bab9a38509bb
Igor Petrenko <igor06.03.1995@gmail.com>
2020-09-26 22:21:43 +0300

    Скорректированы тестовые данные

---
+++ testDump/0005_insert_into_images_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO images (name) VALUES
+('http://lorempixel.com/640/480/fashion'),
+('http://lorempixel.com/640/480/abstract'),
+('http://lorempixel.com/640/480/transport'),
+('http://lorempixel.com/640/480/technics');
--- testDump/0007_insert_into_news_has_image_table.sql
+++ testDump/0007_insert_into_news_has_image_table.sql
@@ -1,5 +1,5 @@
-INSERT INTO images (image_id, news_id) VALUES
-('http://lorempixel.com/640/480/fashion', 1),
-('http://lorempixel.com/640/480/abstract', 1),
-('http://lorempixel.com/640/480/transport', 3),
-('http://lorempixel.com/640/480/technics', 4);
+INSERT INTO news_has_image (news_id, image_id) VALUES
+(1, 1),
+(2, 1),
+(1, 3),
+(3, 4);
